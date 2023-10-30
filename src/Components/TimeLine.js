4e97703af1d0e3525d72149f1603841b145ccd95
DKER2 <pdanghuy03@gmail.com>
2023-10-30 14:01:50 +0800

    Update Experience and Reverse the timeline

--- src/Components/TimeLine.js
+++ src/Components/TimeLine.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import {Dot, ArrowRight} from 'react-bootstrap-icons';
-import styles from './TimeLine.module.css';
 
 function TimeLine(props){
     var timeLine = props.timeLine;
 
     function OneTimeLine(props){
-        timeLine = props.timeLine;
+        var timeLineItem = props.timeLineItem;
         return(
             <div style={{display:"flex", flexDirection:"row"}}>
                 <div style={{flex:"0.2", paddingTop: "19px", fontSize:"16px", fontWeight:"500"}}>
-                    {timeLine[0] + "-" + timeLine[1]} 
+                    {timeLineItem[0] + "-" + timeLineItem[1]} 
                 </div>
                 <div style={{flex:"0.075", borderLeft:"solid 5px #333"}}>
                     <Dot style={{fontSize:"60px", left:"-33px", position:"relative"}}/>
                 </div>
                 <div style={{flex:"0.775", paddingTop: "19px", position:"relative", top:"-9px"}}>
                     <div style={{fontSize:"30px", fontWeight:"500"}}>
-                        {timeLine[2]}
+                        {timeLineItem[2]}
                     </div>
                     <div>
-                        {timeLine[3].map((timeLine, index) => {
+                        {timeLineItem[3].map((timeLineItem, index) => {
                             return(
                                 <div key={index} style={{display:"flex", flexDirection:"row", padding:"15px"}}>
                                     <div style={{marginRight:"20px"}}>
                                         <ArrowRight/>
                                     </div>
                                     <div>
-                                        {timeLine}
+                                        {timeLineItem}
                                     </div>
                                 </div>
                             )
@@ -39,8 +38,8 @@ function TimeLine(props){
     }
     return(
         <div style={{opacity:props.opacity}}>
-            {timeLine.map((timeLine, index) =>{
-                return(<OneTimeLine key={index} timeLine={timeLine}/>)
+            {timeLine.map((timeLineItem, index) =>{
+                return(<OneTimeLine key={index} timeLineItem={timeLineItem}/>)
             })}
         </div>
     )
--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,23 +1,23 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react";
 import Animation from "../Components/Animation";
 import ProfilePicture from '../Components/ProfilePicture';
 import TimeLine from '../Components/TimeLine';
 import './MainPage.css'
 
 function MainPage(props){
-    const [proportion, setProportion] = useState(0);
     const refExperience = useRef(null);
     const refProject = useRef(null);
     const refEducation = useRef(null);
     const refContact = useRef(null);
     var experience = [["Aug 2021","Dec 2021","Hexagon",["Teach a group of 15 students to prepare for Cambridge A-level tests","Prepare homework and study material for student throughout the course"]],
-    ["May 2022","August 2022","Serversam PTE LTD",["Fullstack Developer Internship","Develop and build website using ASP.NET framework","Collaborate with the client to update the product accordingly"]],["Aug 2022", "Present", "URECA Research Programme", ["DNN approaches to speech diarization", "Use BiEncoder Model to predict height and age"]],
-    ["May 2023","August 2023","Continental-NTU Corp Lab",["Deeplearning Research Internship","Do Literature review about multi-task learning","Develop and experiment with different model on Computer Vision tasks"]]]
+    ["May 2022","August 2022","Serversam PTE LTD",["Fullstack Developer Internship","Develop and build website using ASP.NET framework","Collaborate with the client to update the product accordingly"]],
+    ["Aug 2022", "Jun 2023", "URECA Research Programme", ["DNN approaches to speech diarization", "Use BiEncoder Model to predict height and age"]],
+    ["May 2023","August 2023","Continental-NTU Corp Lab",["Deeplearning Research Internship","Do Literature review about multi-task learning","Develop and experiment with different model on Computer Vision tasks"]]].reverse()
     var project=[["Jun 2021","May 2021", "Electrical Field", ["Used  SDL2 to stimulate the Electrical Field to use in teaching Physics"]],
     ["Oct 2021", "March 2022", "Trading Bot",["Work with Binance API to request candlestick data of cryptocurrencies for Machine Learning", "Build up JSON file to store data", "Build database for researching"]],
-    ["May 2022","Jul 2022","Stock Chart Website",["Use ReactJS and Nodejs to build the frontend and backend of the website","Use MongoDB to store the data for website"]]]
+    ["May 2022","Jul 2022","Stock Chart Website",["Use ReactJS and Nodejs to build the frontend and backend of the website","Use MongoDB to store the data for website"]]].reverse()
     var education=[["Aug 2018","May 2021","High School for Gifted Student",["Specialize in Physics", "Overall Score: 9.5/10"]],
-    ["Aug 2021","May 2025","Nanyang Technological University",["Cumulative GPA: 4.56/5.0","Bachelor of Computer Engineering"]]]
+    ["Aug 2021","May 2025","Nanyang Technological University",["Cumulative GPA: 4.56/5.0","Bachelor of Computer Engineering"]]].reverse()
 
     const scrollTo = (scrollDestination) => {
         switch(scrollDestination){
@@ -33,6 +33,8 @@ function MainPage(props){
             case "Contact":
                 refContact.current?.scrollIntoView({ behavior: "smooth" })
                 break
+            default:
+                break
         }
     }
 
@@ -43,7 +45,6 @@ function MainPage(props){
         const position = window.pageYOffset;
         const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
         console.log(position/height);
-        setProportion(position/height);
     };
     
     useEffect(() => {
