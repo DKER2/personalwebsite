ecfe14f6520a7e89f61796586fd5deec44f3e146
DKPHUONG <82434977+DKER2@users.noreply.github.com>
2024-04-26 15:52:40 +0800

    Update Animation.js

--- src/Components/Animation.js
+++ src/Components/Animation.js
@@ -4,7 +4,7 @@ function Animation(props){
     const StringsToRender = ["COMPUTER ENGINEER DEGREE", "WEB DEVELOPER", "NATURAL LANGUAGE PROCESSING"];
     const [stringIndex, setStringIndex] = useState(0);
     function updateStringIndex(){
-        if(stringIndex!=StringsToRender.length-1){
+        if(stringIndex!==StringsToRender.length-1){
             setStringIndex(stringIndex+1);
         }
         else{
