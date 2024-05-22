38eb368b0f1c0ba4e62a711acc1ae4e7992de5f9
DKPHUONG <huyphuon001@e.ntu.edu.sg>
2024-05-22 21:47:15 +0800

    Trying to add logo

--- src/Components/TimeLine.js
+++ src/Components/TimeLine.js
@@ -6,35 +6,69 @@ function TimeLine(props){
 
     function OneTimeLine(props){
         var timeLineItem = props.timeLineItem;
-        return(
-            <div style={{display:"flex", flexDirection:"row"}}>
-                <div style={{flex:"0.2", paddingTop: "19px", fontSize:"16px", fontWeight:"500"}}>
-                    {timeLineItem[0] + "-" + timeLineItem[1]} 
-                </div>
-                <div style={{flex:"0.075", borderLeft:"solid 5px #333"}}>
-                    <Dot style={{fontSize:"60px", left:"-33px", position:"relative"}}/>
-                </div>
-                <div style={{flex:"0.775", paddingTop: "19px", position:"relative", top:"-9px"}}>
-                    <div style={{fontSize:"30px", fontWeight:"500"}}>
-                        {timeLineItem[2]}
-                    </div>
-                    <div>
-                        {timeLineItem[3].map((timeLineItem, index) => {
-                            return(
-                                <div key={index} style={{display:"flex", flexDirection:"row", padding:"15px"}}>
-                                    <div style={{marginRight:"20px"}}>
-                                        <ArrowRight/>
-                                    </div>
-                                    <div>
-                                        {timeLineItem}
-                                    </div>
-                                </div>
-                            )
-                        })}
-                    </div>
-                </div>
+        return (
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <div
+              style={{
+                flex: "0.2",
+                paddingTop: "19px",
+                fontSize: "16px",
+                fontWeight: "500",
+              }}
+            >
+              {timeLineItem[0] + "-" + timeLineItem[1]}
             </div>
-        )
+            {/* <div style={{ flex: "0.1" }}>
+              <img
+                src={
+                  require(`../Assets/CompanyPictures/${timeLineItem[4]}`).default
+                }
+                alt="CompanyLogo"
+                style={{ width: "100px", height: "100px" }}
+              ></img>
+            </div> */}
+            <div style={{ flex: "0.075", borderLeft: "solid 5px #333" }}>
+              <Dot
+                style={{
+                  fontSize: "60px",
+                  left: "-33px",
+                  position: "relative",
+                }}
+              />
+            </div>
+            <div
+              style={{
+                flex: "0.775",
+                paddingTop: "19px",
+                position: "relative",
+                top: "-9px",
+              }}
+            >
+              <div style={{ fontSize: "30px", fontWeight: "500" }}>
+                {timeLineItem[2]}
+              </div>
+              <div>
+                {timeLineItem[3].map((timeLineItem, index) => {
+                  return (
+                    <div
+                      key={index}
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        padding: "15px",
+                      }}
+                    >
+                      <div style={{ marginRight: "20px" }}>
+                        <ArrowRight />
+                      </div>
+                      <div>{timeLineItem}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        );
     }
     return(
         <div style={{opacity:props.opacity}}>
